6ce73fefa0286f2e04228729e9e325d369ad119f
Dan Jansson <dan.jansson@hyxtra.se>
2019-06-17 10:51:22 +0200

    Demo 2 of Inheritance.

--- Academy.ConsoleApp/Demo/InheritanceDemo1.cs
+++ Academy.ConsoleApp/Demo/InheritanceDemo1.cs
@@ -28,6 +28,36 @@ namespace Academy.ConsoleApp.Demo
             Console.WriteLine(human.IsLeftHanded);
             Console.WriteLine(human.LatinName);
             Console.WriteLine(human.NumberOfLegs);
+
+            Cat cat1 = new Cat();
+            cat1.MjauFactor = 127;
+            cat1.NumberOfLegs = 4;
+            cat1.LatinName = "Felis catus";
+
+            Dog dog1 = new Dog();
+            dog1.BarkLevel = 42;
+            dog1.NumberOfLegs = 4;
+            dog1.LatinName = "Canis lupus familiaris";
+
+            Dog dog2 = new Dog();
+            dog2.BarkLevel = 19;
+            dog2.NumberOfLegs = 4;
+            dog2.LatinName = "Canis lupus familiaris";
+
+            // Polymorphism
+            List<Mammal> mammals = new List<Mammal>();
+            mammals.Add(human);
+            mammals.Add(cat1);
+            mammals.Add(dog1);
+            mammals.Add(dog2);
+            // Animal is not inheriting the Mammal class.
+            //mammals.Add(animal);
+
+            foreach (var mammalItem in mammals)
+            {
+                Console.WriteLine(mammalItem.LatinName);
+            }
+
         }
     }
 
@@ -55,4 +85,15 @@ namespace Academy.ConsoleApp.Demo
         public bool IsLeftHanded { get; set; }
     }
 
+    public class Cat : Mammal
+    {
+        public int MjauFactor { get; set; }
+        //public string Breed { get; set; }
+    }
+
+    public class Dog : Mammal
+    {
+        public int BarkLevel { get; set; }
+        //public string Breed { get; set; }
+    }
 }
