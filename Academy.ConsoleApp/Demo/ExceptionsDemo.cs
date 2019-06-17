f4b7a0f9ff56f955cefdbedade7dee54716154a5
Dan Jansson <dan.jansson@hyxtra.se>
2019-06-17 16:15:03 +0200

    Added documentation to the methods in exception demo. Rearranged methods and made code more readable.

--- Academy.ConsoleApp/Demo/ExceptionsDemo.cs
+++ Academy.ConsoleApp/Demo/ExceptionsDemo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Academy.ConsoleApp.Demo
@@ -7,6 +8,61 @@ namespace Academy.ConsoleApp.Demo
     public class ExceptionsDemo
     {
         public void Run()
+        {
+            //Demo1();
+
+            //Demo2();
+
+            //Demo3();
+        }
+
+        /// <summary>
+        /// This method will demonstrate UnauthorizedAccessException.
+        /// The reason that this exception occur is because of the user 
+        /// doesn't have access to create files or write to the root in C:\
+        /// </summary>
+        private void Demo3()
+        {
+            try
+            {
+                File.CreateText(@"C:\exceptionstest");
+            }
+            catch (UnauthorizedAccessException uaex)
+            {
+                Console.WriteLine($"An access related error occured: {uaex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// This demo demonstrates the division by zero exception that will occur if you try to divide something with zero.
+        /// Note that the data type need to be int.
+        /// </summary>
+        private void Demo2()
+        {
+            int totalAmount = 135;
+
+            Console.WriteLine("Enter a number please:");
+            string myOtherNumberString = Console.ReadLine();
+            int.TryParse(myOtherNumberString, out int myOtherParsedNumber);
+
+            try
+            {
+                int result = totalAmount / myOtherParsedNumber;
+            }
+            catch (DivideByZeroException dex)
+            {
+                Console.WriteLine("Division med noll!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// This demo demonstrates what will happen if you try to parse a string for a number, but the string is invalid.
+        /// </summary>
+        private void Demo1()
         {
             Console.WriteLine("Enter a number please:");
             string myNumberString = Console.ReadLine();
@@ -21,15 +77,6 @@ namespace Academy.ConsoleApp.Demo
             {
                 Console.WriteLine("An error occured when parsing the the input.");
             }
-
-            int totalAmount = 135;
-
-            Console.WriteLine("Enter a number please:");
-            string myOtherNumberString = Console.ReadLine();
-            int.TryParse(myOtherNumberString, out int myOtherParsedNumber);
-
-            int result = totalAmount / myOtherParsedNumber;
-
         }
     }
 }
