3a9d27fbe8c370fa3f1c9b531235c71505c26974
Dan Jansson <dan.jansson@hyxtra.se>
2019-06-18 11:15:13 +0200

    Implemented the ParseAnimals method.

--- Academy.ConsoleApp/Demo/ExceptionsDemo2.cs
+++ Academy.ConsoleApp/Demo/ExceptionsDemo2.cs
@@ -16,65 +16,87 @@ namespace Academy.ConsoleApp.Demo
                 Console.WriteLine("Enter some animals separated by comma:");
                 string animalsString = Console.ReadLine();
 
-                // Split string into array of strings.
-                string[] animals = animalsString.Split(',');
+                // Invoke the method ParseAnimals to check the string and return the animals in an array.
+                string[] animals = ParseAnimals(animalsString);
 
-                // Validate the input and check that everything is ok and according to the rules that where defined in the exercise.
-                try
+                // Display all animals
+                int animalNumber = 1;
+                foreach (var animal in animals)
                 {
-                    // Check if this is an empty string.
-                    if (string.IsNullOrWhiteSpace(animalsString))
-                    {
-                        throw new ArgumentException("Animal string doesn't contain any letters.");
-                    }
-
-                    // Test all animal names individually
-                    foreach (var animal in animals)
-                    {
-                        if (string.IsNullOrWhiteSpace(animal))
-                        {
-                            throw new ArgumentException("One of the animals didn't contain any letters.");
-                        }
-                        if (animal.Length > 20)
-                        {
-                            throw new ArgumentException($"The animal '{animal}' has too many letters.");
-                        }
-                        Regex regex = new Regex(@"[^\w]+");
-                        if (regex.IsMatch(animal))
-                        {
-                            throw new ArgumentException($"The animal '{animal}' contains invalid letters.");
-                        }
-                        if (animal.Equals("academy", StringComparison.CurrentCultureIgnoreCase))
-                        {
-                            throw new AcademyException("Animals cannot be named Academy.");
-                        }
-                    }
-
-                    // Display all animals
-                    int animalNumber = 1;
-                    foreach (var animal in animals)
-                    {
-                        Console.WriteLine($"Animal {animalNumber}: {animal}");
-                        animalNumber++;
-                    }
-
-                    break;
+                    Console.WriteLine($"Animal {animalNumber}: {animal}");
+                    animalNumber++;
                 }
-                catch (AcademyException aex)
+
+                break;
+            }
+        }
+
+        /// <summary>
+        /// This method splits a string using the comma character and returns an array of strings.
+        /// If any of the array items is empty or contains invalid characters an exception will be thrown.
+        /// </summary>
+        /// <param name="animalsString">The string with comma-separated strings.</param>
+        /// <returns>An array with string items.</returns>
+        private string[] ParseAnimals(string animalsString)
+        {
+            // Split string into array of strings.
+            string[] animals = animalsString.Split(',');
+
+            // Validate the input and check that everything is ok and according to the rules that where defined in the exercise.
+            try
+            {
+                // Check if this is an empty string.
+                if (string.IsNullOrWhiteSpace(animalsString))
                 {
-                    // Logga till externt system...
-                    Console.WriteLine(aex.Message);
+                    throw new ArgumentException("Animal string doesn't contain any letters.");
                 }
-                catch (ArgumentException aex)
+
+                // Test all animal names individually
+                foreach (var animal in animals)
                 {
-                    Console.WriteLine(aex.Message);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    throw;
+                    if (string.IsNullOrWhiteSpace(animal))
+                    {
+                        throw new ArgumentException("One of the animals didn't contain any letters.");
+                    }
+                    if (animal.Length > 20)
+                    {
+                        throw new ArgumentException($"The animal '{animal}' has too many letters.");
+                    }
+                    Regex regex = new Regex(@"[^\w]+");
+                    if (regex.IsMatch(animal))
+                    {
+                        throw new ArgumentException($"The animal '{animal}' contains invalid letters.");
+                    }
+                    // This exception will occur if the string item equals the word 'academy'.
+                    if (animal.Equals("academy", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        throw new AcademyException("Animals cannot be named Academy.");
+                    }
                 }
             }
+            //////////////////////////////////////////////////
+            // NOTE: The order of the catch statements matter a lot and you should put the most significant exception first.
+            //////////////////////////////////////////////////
+            // Will be catched if the thrown exception is of type or inherits from AcademyException.
+            catch (AcademyException aex)
+            {
+                // Logga till externt system...
+                Console.WriteLine(aex.Message);
+            }
+            // Will be catched if the thrown exception is of type or inherits from ArgumentException.
+            catch (ArgumentException aex)
+            {
+                Console.WriteLine(aex.Message);
+            }
+            // Will be catched if the thrown exception is of type or inherits from Exception.
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+
+            // Return the string array to the calling context.
+            return animals;
         }
     }
 }
