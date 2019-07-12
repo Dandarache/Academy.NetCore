c7f8a4034ed1200c0a92857bf0efdecfc3500db7
Dan Jansson <dan.jansson@hyxtra.se>
2019-07-12 11:23:15 +0200

    Removed redundant code from Samurai project.

---
+++ C#/LinqDemos/LinqDemos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
