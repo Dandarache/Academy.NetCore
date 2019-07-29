1e8f5dcbf0747b4e3ff2197935cf8271d3466685
Dan Jansson <dan.jansson@hyxtra.se>
2019-07-29 13:46:28 +0200

    Added HTML, CSS and JavaScript project with exercises.

---
+++ Frontend/FrontendDemos/FrontendDemos/wwwroot/javascriptintro/3 - Loops.js
@@ -0,0 +1,94 @@
+﻿
+function loop1() {
+
+    /*
+        Använd en for-loop för att skriva ut alla siffror mellan 5 och 18
+    */
+
+
+}
+
+function loop2() {
+
+    /*
+        Använd en for-loop för att skriva ut alla siffror mellan 0 och 9
+    */
+
+
+}
+
+function loop3() {
+
+    /*
+        Använd en forloop för att skriva ut:
+            "Siffran 4 är tjusig". 
+            "Siffran 5 är tjusig"
+            "Siffran 6 är tjusig"
+            "Siffran 7 är tjusig"
+            "Siffran 8 är tjusig"
+    */
+
+
+
+    /* Extra: använd en forloop för att skriva ut:
+        "4:a"
+        "5:a"
+        "6:a"
+        "7:a"
+        "8:a"
+    */
+}
+
+function loop4() {
+
+    /*
+        Lös föregående uppgift men använd en while-loop
+    */
+
+
+}
+
+function loop5() {
+
+    /*
+	    Skapa en array "importantYears" med årtalen 1492, 1789, 1859, 1929
+        Använd "for of" för att skriva ut alla årtal
+    */
+
+
+
+    /*
+    Extra: 
+
+        Skriv ut "Året är 1492", "Året är 1789" ...
+
+        skapa en array med färger "blå", "röd", "grön", "gul", "lila". Använd "for of" för att skriva ut alla färger 
+
+    */
+}
+
+function loop6() {
+
+    /*
+	    Skapa en array "importantYears" med årtalen 1492, 1789, 1859, 1929
+	    Använd "for of" för att skriva ut alla årtal med en siffra till vänster:
+	    1) 1492
+	    2) 1789
+	    3) 1859
+	    4) 1929
+    */
+
+
+}
+
+function loop7() {
+
+    /*
+	    Samma som sist men avbryt loopen om årtalet är högre än 1800. Ska alltså ge:
+	    1) 1492
+	    2) 1789
+    */
+
+
+
+}
---
+++ Frontend/FrontendDemos/FrontendDemos/wwwroot/js/main.js
@@ -0,0 +1,10 @@
+﻿function changeHeader() {
+
+    document.getElementById('academyHeader').innerHTML = "Hello Academy Trafikverket MODIFIED!";
+
+    //alert(document.getElementById('academyHeader'));
+}
+
+function changeColor(colorName, id) {
+    document.getElementById(id).style.color = colorName;
+}
