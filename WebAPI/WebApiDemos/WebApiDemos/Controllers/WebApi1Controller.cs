385145af25cbbceb32aa49860fcff0656727cd00
Dan Jansson <dan.jansson@hyxtra.se>
2019-08-12 10:40:05 +0200

    Refactored method for ParsePlanet.

--- WebAPI/WebApiDemos/WebApiDemos/Controllers/WebApi1Controller.cs
+++ WebAPI/WebApiDemos/WebApiDemos/Controllers/WebApi1Controller.cs
@@ -23,24 +23,10 @@ namespace WebApiDemos.Controllers
             }
 
             // Du behöver göra "Planet"-klassen och metoden "ParsePlanet"
-            Planet planet = ParsePlanet(formContent);
+            Planet planet = Planet.ParsePlanet(formContent);
             return Ok($"Ny planet {planet.Name} skapad med storleken {planet.Size}.");
         }
 
-        private Planet ParsePlanet(string formContent)
-        {
-            Regex regex1 = new Regex("(&|^)Name=([^&$]*)");
-            var planetName = regex1.Match(formContent).Groups[2].Value;
 
-            Regex regex2 = new Regex("(&|^)Size=([^&$]*)");
-            var planetSizeString = regex2.Match(formContent).Groups[2].Value;
-            int.TryParse(planetSizeString, out int planetSize);
-
-            return new Planet
-            {
-                Name = planetName,
-                Size = planetSize
-            };
-        }
     }
 }
--- WebAPI/WebApiDemos/WebApiDemos/Models/Planet.cs
+++ WebAPI/WebApiDemos/WebApiDemos/Models/Planet.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace WebApiDemos.Models
@@ -9,5 +10,21 @@ namespace WebApiDemos.Models
     {
         public string Name { get; set; }
         public int Size { get; set; }
+
+        public static Planet ParsePlanet(string planetData)
+        {
+            Regex regex1 = new Regex("(&|^)Name=([^&$]*)");
+            var planetName = regex1.Match(planetData).Groups[2].Value;
+
+            Regex regex2 = new Regex("(&|^)Size=([^&$]*)");
+            var planetSizeString = regex2.Match(planetData).Groups[2].Value;
+            int.TryParse(planetSizeString, out int planetSize);
+
+            return new Planet
+            {
+                Name = planetName,
+                Size = planetSize
+            };
+        }
     }
 }
