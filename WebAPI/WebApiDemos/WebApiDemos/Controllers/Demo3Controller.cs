32d502554fd617c80e845fa1712259a6a1a9f21d
Dan Jansson <dan.jansson@hyxtra.se>
2019-08-12 14:34:54 +0200

    Added demo 3 with GET and POST.

---
+++ WebAPI/WebApiDemos/WebApiDemos/Controllers/Demo3Controller.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDemos.Controllers
+{
+    [Route("demo3")]
+    public class Demo3Controller : Controller
+    {
+        [HttpGet("CheckMemberGet")]
+        //[Route("CheckMemberGet")]
+        public IActionResult CheckMemberGet(
+            string username, 
+            bool isMember, 
+            string firstName, 
+            string lastName)
+        {
+            return Ok("Det gick bra!");
+        }
+
+        [HttpPost("CheckMemberPost")]
+        public IActionResult CheckMemberPost(
+            string username,
+            bool isMember,
+            string firstName,
+            string lastName)
+        {
+            return Ok("Det gick bra!");
+        }
+
+    }
+}
