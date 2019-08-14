63d0ca836f1b85c74817f39ba3c720f6776a7f69
Dan Jansson <dan.jansson@hyxtra.se>
2019-08-14 11:05:35 +0200

    Added table fragment for news listing.

--- WebAPI/NewsApp/wwwroot/js/main.js
+++ WebAPI/NewsApp/wwwroot/js/main.js
@@ -1,1 +1,23 @@
 ﻿
+async function clickRecreate() {
+    //alert("clickRecreate()");
+
+    let recreateButton = document.getElementById('recreateButton');
+    recreateButton.disabled = true;
+
+    let response = await fetch('api/news/recreate', {
+        method: 'POST'
+    });
+
+    if (response.status === 204) {
+        alert("Database Recreated");
+
+        // ...
+    }
+    else {
+        console.log('Något vart fel: ' + response);
+    }
+
+    recreateButton.disabled = false;
+
+}
