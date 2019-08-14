12ff8d230235ffcf887440f003ba1f57be3d4518
Dan Jansson <dan.jansson@hyxtra.se>
2019-08-14 15:37:21 +0200

    Added method for adding news.

--- WebAPI/NewsApp/wwwroot/js/main.js
+++ WebAPI/NewsApp/wwwroot/js/main.js
@@ -2,6 +2,57 @@
 
 let allNews = [];
 
+function clickShowAddNews() {
+
+    let addForm = document.getElementById('addArea');
+    addForm.style.display = 'block';
+
+}
+
+async function clickAddNews() {
+
+    // POST /api/news/add
+
+    //Header: <input id="addAreaHeader" type="text" />
+    //Category: <select id="addAreaCategory"></select>
+    //Intro: <input id="addAreaIntro" type="text" />
+    //Body: <input id="addAreaBody" type="text" />
+
+    alert(12);
+
+    let newNews = {};
+    newNews.header = document.getElementById('addAreaHeader').value;
+    newNews.intro = document.getElementById('addAreaIntro').value;
+    newNews.body = document.getElementById('addAreaBody').value;
+
+    console.log(newNews);
+
+    let response = await fetch('api/news', {
+        method: 'POST',
+        body: JSON.stringify(newNews),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.status === 200) {
+        document.getElementById('addAreaHeader').value = '';
+        document.getElementById('addAreaIntro').value = '';
+        document.getElementById('addAreaBody').value = '';
+
+        let addForm = document.getElementById('addArea');
+        addForm.style.display = 'hidden';
+    }
+    else if (response.status === 400) {
+        console.log('Invalid input data.');
+    }
+
+    getAllNews();
+    render();
+
+}
+
+
 async function clickRecreate() {
     //alert("clickRecreate()");
 
@@ -52,21 +103,21 @@ function clickStatArea() {
     render();
 }
 
-function clickAddNews() {
-    (async () => {
-        const rawResponse = await fetch('https://httpbin.org/post', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ a: 1, b: 'Textual content' })
-        });
-        const content = await rawResponse.json();
+//function clickAddNews() {
+//    (async () => {
+//        const rawResponse = await fetch('https://httpbin.org/post', {
+//            method: 'POST',
+//            headers: {
+//                'Accept': 'application/json',
+//                'Content-Type': 'application/json'
+//            },
+//            body: JSON.stringify({ a: 1, b: 'Textual content' })
+//        });
+//        const content = await rawResponse.json();
 
-        console.log(content);
-    })();
-}
+//        console.log(content);
+//    })();
+//}
 
 function render() {
     let html = "<table>";
