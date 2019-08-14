5640e7a04b5b8c2505651bfda9430028cc8d42de
Dan Jansson <dan.jansson@hyxtra.se>
2019-08-14 13:39:24 +0200

    Added getAllNews method. Added render method.

--- WebAPI/NewsApp/wwwroot/js/main.js
+++ WebAPI/NewsApp/wwwroot/js/main.js
@@ -1,4 +1,7 @@
 ﻿
+
+let allNews = [];
+
 async function clickRecreate() {
     //alert("clickRecreate()");
 
@@ -20,4 +23,65 @@ async function clickRecreate() {
 
     recreateButton.disabled = false;
 
+}
+
+async function clickSeed() {
+
+    let seedButton = document.getElementById('seedButton');
+    seedButton.disabled = true;
+
+    let response = await fetch('api/news/seed', {
+        method: 'POST'
+    });
+
+    if (response.status === 204) {
+        alert("Data added by seed function.");
+
+        // ...
+    }
+    else {
+        console.log('Something went wrong: ' + response);
+    }
+
+    seedButton.disabled = false;
+
+}
+
+function clickStatArea() {
+    getAllNews();
+    render();
+}
+
+function render() {
+
+    let html = '';
+
+    for (let news of allNews) {
+        html += '<b>' + news.header + '</b><br>';
+        console.log(news.header);
+    }
+
+    console.log(html);
+
+}
+
+async function getAllNews() {
+    //alert('getAllNews()');
+
+    let response = await fetch('api/news', {
+        method: 'get'
+    });
+
+    if (response.status === 200) {
+
+        let newsList = await response.json();
+
+        //let first = newsList[0];
+        //alert(first);        
+
+        allNews = newsList;
+    }
+    else {
+        console.log('Something went wrong: ' + response);
+    }
 }
