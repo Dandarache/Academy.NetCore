6f51d1b26c9be1f355039ada21b4b08efba1cdb9
Dan Jansson <dan.jansson@hyxtra.se>
2019-08-14 14:50:28 +0200

    Added rendering of news table.

--- WebAPI/NewsApp/wwwroot/js/main.js
+++ WebAPI/NewsApp/wwwroot/js/main.js
@@ -52,17 +52,39 @@ function clickStatArea() {
     render();
 }
 
+function clickAddNews() {
+    (async () => {
+        const rawResponse = await fetch('https://httpbin.org/post', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ a: 1, b: 'Textual content' })
+        });
+        const content = await rawResponse.json();
+
+        console.log(content);
+    })();
+}
+
 function render() {
-
-    let html = '';
-
+    let html = "<table>";
+    html += `<tr><th>Id</th><th>Header</th><th>Category</th><th>Created</th><th>Updated</th><th>Update</th><th>Delete</th></tr>`;
     for (let news of allNews) {
-        html += '<b>' + news.header + '</b><br>';
-        console.log(news.header);
+        html += `<tr>
+                    <td>${news.id}</td>
+                    <td>${news.header}</td>
+                    <td>${news.category === null ? "" : news.category.name}</td>
+                    <td>${news.createdDate}</td>
+                    <td>${news.updatedDate}</td>
+                    <td><button onclick="showUpdateNewsForm(${news.id})">Update</button></td>
+                    <td><button onclick="deleteNews(${news.id})">X</button></td>
+                 </tr>`;
     }
+    html += "</table>";
 
-    console.log(html);
-
+    document.getElementById("newsList").innerHTML = html;
 }
 
 async function getAllNews() {
