b48fade74b4642eb2a451f47891146dc2200d6b4
Dan Jansson <dan.jansson@hyxtra.se>
2019-08-19 10:55:09 +0200

    Added click events for seed and re-create.

--- Angular/NewsAppAngular/wwwroot/js/main-angular.js
+++ Angular/NewsAppAngular/wwwroot/js/main-angular.js
@@ -1,1 +1,11 @@
 ﻿
+var app = angular.module('newsApp', []);
+app.controller('myController', function ($scope) {
+    $scope.clickSeed = function () {
+        alert('clickSeed clicked!');
+    };
+    $scope.clickStatArea = function () {
+        alert('clickStatArea clicked!');
+    };
+});
+
