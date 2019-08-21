4ec0793b618170b98ab158d12f56a927f54b34ba
Dan Jansson <dan.jansson@hyxtra.se>
2019-08-21 18:46:12 +0200

    Added horse images. Added click on image event using checkbox. Added datagrid to WPF demo.

---
+++ WPF/FirstWPF/FirstWPF/Horse.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Controls;
+
+namespace FirstWPF
+{
+    public class Horse
+    {
+        public bool EyesOpen { get; internal set; }
+        public Image Image { get; set; }
+    }
+}
--- WPF/FirstWPF/FirstWPF/MainWindow.xaml.cs
+++ WPF/FirstWPF/FirstWPF/MainWindow.xaml.cs
@@ -20,9 +20,70 @@ namespace FirstWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly BitmapImage openEyes = new BitmapImage(new Uri(@"\Images\horse.png", UriKind.Relative));
+        readonly BitmapImage closedEyes = new BitmapImage(new Uri(@"\Images\horse-closed.png", UriKind.Relative));
+
+        Horse horse1;
+        Horse horse2;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            dataGrid1.AutoGenerateColumns = true;
+            dataGrid1.ItemsSource =
+                EmployeeRepository.GetEmployees().Select(a =>
+                    new
+                    {
+                        FirstName = a.FirstName,
+                        LastName = a.LastName,
+                        Gender = a.Gender.ToString()
+                    });
+
+            horse1 = new Horse { EyesOpen = true, Image = imageHorse1 };
+            horse2 = new Horse { EyesOpen = true, Image = imageHorse2 };
+        }
+
+        private void chkbox1_Checked(object sender, RoutedEventArgs e)
+        {
+            SetImage(horse1);
+        }
+
+        private void chkbox1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            SetImage(horse1);
+        }
+
+        private void chkbox2_Checked(object sender, RoutedEventArgs e)
+        {
+            SetImage(horse2);
+        }
+
+        private void chkbox2_Unchecked(object sender, RoutedEventArgs e)
+        {
+            SetImage(horse2);
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            SetImage(horse1);
+            SetImage(horse2);
+        }
+
+        private void SetImage(Horse horse)
+        {
+            horse.Image.Source = horse.EyesOpen ? closedEyes : openEyes;
+            horse.EyesOpen = !horse.EyesOpen;
+        }
+
+        private void SummarizeButton(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(Value1.Text, out int val1);
+            int.TryParse(Value2.Text, out int val2);
+
+            int sum = val1 + val2;
+
+            SumValue.Text = sum.ToString();
         }
     }
 }
