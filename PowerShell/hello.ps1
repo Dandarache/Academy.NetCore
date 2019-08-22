af827dc54799500f694e0e4d6a6c72fb7d73ba39
Dan Jansson <dan.jansson@hyxtra.se>
2019-08-22 20:02:14 +0200

    Added PowerShell exercises and solutions.

---
+++ PowerShell/hello.ps1
@@ -0,0 +1,15 @@
+﻿param (
+  [string]$firstname="Sven",
+  [int]$age=50,
+  [string]$toupper="no"
+)
+
+$apa=123 # skrivs inte ut
+
+$text = "Hej " + $firstname + "! Om 30 år är du " + ($age+30) + " år gammal"
+
+if ($toupper -eq "yes"){
+    $text.ToUpper()
+} else {
+    $text
+}
